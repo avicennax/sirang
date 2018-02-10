fec5b4e8f2c340a726085161fa53aabc102bb9ce
Simon Haxby <simon.haxby@gmail.com>
2018-02-09 20:02:06 -0800

    sirang tested on rnn-pg

--- sirang.py
+++ sirang.py
@@ -4,21 +4,27 @@ import subprocess
 
 class Sirang(object):
     def __init__(self, host='mongodb://localhost:27017', verbose=0):
-        self.client = MongoClient(host)
+        self.client = pymongo.MongoClient(host)
         self.dbs = {}
         self.verbose = verbose
 
-    def store_meta(db_name, doc=None, doc_id=None):
+    def db_doc_count(self, db_name):
+        return self.get_db(db_name).posts.count()
+
+    def store_meta(self, db_name, doc=None, doc_id=None):
         if doc is None:
             doc = {}
             
-        dt_now = datetime.datetime.now().str()
+        dt_now = str(datetime.datetime.now())
         git_commit = subprocess.check_output(["git", "describe", "--always"]).strip()
         doc.update({'exe-date': dt_now, 'git-commit': git_commit})
         if doc_id:
             doc["_id"] = doc_id
+        else:
+            doc["_id"] = self.db_doc_count(db_name) + 1
 
-        self.store(db_name=db_name, raw_document=doc, store={}, inversion=True)
+        res_id = self.store(db_name=db_name, raw_document=doc, store={}, inversion=True)
+        return res_id
 
     def get_db(self, db_name):
         if db_name not in self.dbs.keys():
@@ -36,8 +42,10 @@ class Sirang(object):
         if doc_id:
             doc['_id'] = doc_id
 
-        result = posts.insert_one(post)
-        self._verbose_print(result)
+        result = posts.insert_one(doc)
+        res_id = str(result.inserted_id)
+        self._verbose_print(res_id)
+        return res_id
 
     def retrieve(self, db_name, filter):
         db = self.get_db(db_name)
@@ -46,7 +54,7 @@ class Sirang(object):
         self._verbose_print(retrieved_params)
         return retrieved_doc
 
-    def dstore(self, db_name, store, inversion=False, doc_id=None):
+    def dstore(self, db_name, store, inversion=False, doc_id=None, store_return=False):
         db = self.get_db(db_name)
         posts = db.posts
         new_post = {}
@@ -57,10 +65,15 @@ class Sirang(object):
                         new_post[param_name] = param
                     if doc_id:
                         new_post['_id'] = doc_id
-                result = posts.insert_one(post)
-                self._verbose_print(result)
-
-                return f(*args, **kwargs)
+                if store_return:
+                    res_store, res = f(*args, **kwargs)
+                    new_post.update(res_store)
+                else:
+                    res = f(*args, **kwargs)
+                result = posts.insert_one(new_post)
+                res_id = str(result.inserted_id)
+                self._verbose_print(res_id)
+                return res
             return func
         return store_dec
 
@@ -76,16 +89,16 @@ class Sirang(object):
             return func
         return store_dec
 
-    def _include(param_name, store_list, inversion):
+    def _include(self, param_name, store_list, inversion):
         if inversion:
             return param_name not in store_list
         return param_name in store_list
 
-    def _doc_sub_dict(raw_doc, store, inversion):
+    def _doc_sub_dict(self, raw_doc, store, inversion):
         if inversion:
-            return {key: pair for key, pair in raw.doc.items() if key not in store}
-        return {key: pair for key, pair in raw.doc.items() if key in store}
+            return {key: pair for key, pair in raw_doc.items() if key not in store}
+        return {key: pair for key, pair in raw_doc.items() if key in store}
 
-    def _verbose_print(pstr):
+    def _verbose_print(self, pstr):
         if self.verbose == 1:
             print(pstr)

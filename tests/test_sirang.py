d75347208869060f510eb1380f025760cb899d77
avicennax <simon.haxby@gmail.com>
2018-07-10 23:42:09 -0700

    DEV base tests added

--- tests/test_sirang.py
+++ tests/test_sirang.py
@@ -1,17 +1,26 @@
+import logging
+import pytest
 import os
+import shutil
 import sys
+import tempfile
 from subprocess import CalledProcessError, check_call
 
 from sirang import Sirang
 
 
 @pytest.fixture(scope="session", autouse=True)
-def setup_server():
+def setup_server(request):
     """Setup server."""
-    setup_server_call = ['mongod', '--fork', '--logpath', tmpdir, '--db-path', tmpdir]
+    tmpdir = tempfile.mkdtemp()
+    tmplog = os.path.join(tmpdir, 'log')
+    setup_server_call = [
+        'mongod', '--fork', '--logpath', tmplog, '--dbpath', tmpdir]
     try:
         check_call(setup_server_call)
-    except CalledProcessError:
+        # Removing the DB directory will cause crash the Mongo daemon
+        request.addfinalizer(lambda: shutil.rmtree(tmpdir))
+    except OSError:
         logging.error("mongod setup failed; aborting tests")
         sys.exit(1)
 
@@ -22,4 +31,4 @@ def sirang_instance():
 
 
 def test_sirang(sirang_instance):
-    assert 0 == sirang_instance('db', 'posts', {})
+    assert '0' == sirang_instance.store('db', 'posts', {'_id': 0})
